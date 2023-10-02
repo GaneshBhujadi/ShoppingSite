34708123fe911cdc162d7f3bb73205d7e2cb130b
Manoj Bhujadi <manojbhujadi96k@gmail.com>
2023-10-02 18:17:17 +0530

    first commit

---
+++ webToleadScript.js
@@ -0,0 +1,5 @@
+function submitHandler(){
+    let inputDate = document.querySelector('.inputDate');
+    let outputDate = document.querySelector('.outputDate');
+    console.log('inputDate',inputDate.value);
+}
